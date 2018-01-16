53a0cda67fd094d1fe5e8b85e276c86579baae6c
NicoHood <git@nicohood.de>
2018-01-16 14:43:24 +0100

    Only send CDC Serial data once the DTR line is set.

--- lib/USB_CDC_SERIAL/src/usb_cdc_serial.c
+++ lib/USB_CDC_SERIAL/src/usb_cdc_serial.c
@@ -54,6 +54,24 @@ USB_ClassInfo_CDC_Device_t VirtualSerial_CDC_Interface =
 			},
 	};
 
+static bool HostReady = false;
+
+/** CDC class driver callback function the processing of changes to the virtual
+ *  control lines sent from the host..
+ *
+ *  \param[in] CDCInterfaceInfo  Pointer to the CDC class interface configuration structure being referenced
+ */
+void EVENT_CDC_Device_ControLineStateChanged(USB_ClassInfo_CDC_Device_t *const CDCInterfaceInfo)
+{
+	/* You can get changes to the virtual CDC lines in this callback; a common
+	   use-case is to use the Data Terminal Ready (DTR) flag to enable and
+	   disable CDC communications in your application when set to avoid the
+	   application blocking while waiting for a host to become ready and read
+	   in the pending data from the USB endpoints.
+	*/
+	HostReady = (CDCInterfaceInfo->State.ControlLineStates.HostToDevice & CDC_CONTROL_LINE_OUT_DTR) != 0;
+}
+
 #define TX_RX_LED_PULSE_MS 3
 static uint8_t rx_led_count = 0;
 static uint8_t tx_led_count = 0;
@@ -90,7 +108,10 @@ static int usb_cdc_serial_fputc(char c, FILE* stream)
 {
     TX_LED_ON();
     tx_led_count = TX_RX_LED_PULSE_MS;
-	return CDC_Device_SendByte(&VirtualSerial_CDC_Interface, c) ? _FDEV_ERR : 0;
+	if (!HostReady) {
+		return _FDEV_ERR;
+	}
+	return CDC_Device_SendByte(&VirtualSerial_CDC_Interface, c) ? _FDEV_ERR : c;
 }
 
 static int usb_cdc_serial_fgetc(FILE* stream)
